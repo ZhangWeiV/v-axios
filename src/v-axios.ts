0683bf85adf56f50eb73805bdcdafbcf3e4bb8ba
zhangv <958365882@qq.com>
2019-05-30 11:40:47 +0800

    测试

--- src/v-axios.ts
+++ src/v-axios.ts
@@ -1,4 +1,9 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+export default class DummyClass {
+  private age: number;
+  constructor(){
+    this.age = 666;
+  }
+}
